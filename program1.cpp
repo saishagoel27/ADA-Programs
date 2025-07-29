a3d38728e6f06be5817953a2b08fd19eb13dffd6
Saisha Goel <saishagoel005@gmail.com>
2025-07-30 00:22:29 +0530

    Initial Commit

---
+++ program1.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+void BinarySearch (int arr[], int size, int key)
+{
+    int ub, lb, mid;
+    ub=size-1;
+    lb=0;
+    int flag=0;
+    while (lb<=ub)
+    {
+        mid=lb+((ub-lb)/2);
+        if (key==arr[mid])
+        {
+         flag=1;
+         cout<<"Found"<<endl;
+         break;
+        }
+        else if (arr[mid]<key)
+        {
+            lb=mid+1;
+        }
+        else
+        {
+            ub=mid-1;
+        }
+    }
+    if (flag!=1)
+    {
+        cout<<"Not Found"<<endl;
+    }
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+            }
+        }
+        int t = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = t;
+        
+        int pi = i + 1;
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int counter=5;
+    while (counter>0)
+    {
+        int size;
+        cout<<"Enter size: ";
+        cin>>size;
+    
+        srand(time(0));
+        int arr[size];
+    
+        for (int i=0; i<size; i++)
+        {
+            arr[i]=rand() % 1000 + 1;
+        }
+    
+        quickSort(arr, 0, size - 1);
+        for (int i= 0; i<size; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    
+        int key;
+        cout<<"Enter key: ";
+        cin>>key;
+        
+        auto start=high_resolution_clock::now();
+            BinarySearch(arr, size, key);
+        auto stop=high_resolution_clock::now();
+    
+        auto duration=duration_cast<microseconds>(stop-start);
+        cout<<"Time in microseconds: "<<duration.count()<<endl;
+        counter--;
+    }
+    return 0;
+}
---
+++ program2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+            }
+        }
+        int t = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = t;
+
+        int pi = i + 1;
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int counter = 5;
+    while (counter > 0)
+    {
+        int size;
+        cout << "Enter size: ";
+        cin >> size;
+
+        srand(time(0));
+        int arr[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = rand() % 1000 + 1;
+        }
+        auto start = high_resolution_clock::now();
+            quickSort(arr, 0, size - 1);
+         auto stop = high_resolution_clock::now();
+        cout << "Sorted Array:"<<endl;
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        auto duration = duration_cast<nanoseconds>(stop - start);
+        cout << "Time in nanoseconds: " << duration.count() << endl;
+        counter--;
+    }
+    return 0;
+}
